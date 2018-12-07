314bd84405ddfc2912a46b94f7b46275867ff116
Mary Goodhart <marya.grace5@gmail.com>
2018-12-07 10:34:30 -0700

    User story 2

--- spec/features/user_can_see_list_of_astronauts_spec.rb
+++ spec/features/user_can_see_list_of_astronauts_spec.rb
@@ -11,8 +11,20 @@ describe 'When I visit /astronauts' do
     expect(page).to have_content(astronaut_1.name)
     expect(page).to have_content(astronaut_1.age)
     expect(page).to have_content(astronaut_1.job)
-    expect(page).to have_content(astronaut_1.name)
-    expect(page).to have_content(astronaut_1.age)
-    expect(page).to have_content(astronaut_1.job)    
+    expect(page).to have_content(astronaut_2.name)
+    expect(page).to have_content(astronaut_2.age)
+    expect(page).to have_content(astronaut_2.job)
+    expect(page).to have_content(astronaut_3.name)
+    expect(page).to have_content(astronaut_3.age)
+    expect(page).to have_content(astronaut_3.job)
+  end
+  it "should see the average age of all astronauts." do
+    astronaut_1 = Astronaut.create(name: "astronaut_1", age: 35, job: "Captain")
+    astronaut_2 = Astronaut.create(name: "astronaut_2", age: 45, job: "Engineer")
+    average_age = 40
+
+    visit '/astronauts'
+
+    expect(page).to have_content("Average Age: #{average_age}")
   end
 end
--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,14 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+  describe 'Class Methods' do
+    describe '.average_age' do
+      it 'returns average age for all astronauts' do
+        Astronaut.create(name: "astronaut_1", age: 35, job: "Captain")
+        Astronaut.create(name: "astronaut_2", age: 45, job: "Engineer")
+
+        expect(Astronaut.average_age).to eq(40)
+      end
+    end
+  end
 end
